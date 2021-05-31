2a18a10fd5a6882fe725d2f504332cf644b0c43e
sukeshnibakshi29 <82715100+sukeshnibakshi29@users.noreply.github.com>
2021-05-31 01:42:32 -1200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var position;
 var database;
 
 function preload(){
-   bg =loadImage("cityImage.png");
+   bg = loadImage("cityImage.png");
    balloonImage1=loadAnimation("hotairballoon1.png");
    balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
    "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
@@ -13,18 +13,20 @@ function preload(){
 
 //Function to set initial environment
 function setup(){
+  createCanvas(1800,500);
   database = firebase.database()
   console.log(database);
-  createCanvas(1500,700);
+  
 
   balloon = createSprite(250,450,150,150);
-  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.addAnimation("hotAirBalloon",balloonImage2);
   balloon.scale=0.5;
 
   var balloonPosition = database.ref('balloon/height');
-  balloonPosition.on("value",readPosition,showError);
+  balloonPosition.on("value",readHeight,showError);
 
-  textSize(20); 
+  
+textSize(20); 
 }
 
 // function to display UI
@@ -34,28 +36,24 @@ function draw() {
   if(keyDown(LEFT_ARROW)){
     updateHeight(-10,0);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in left direction
-    balloon.scale = balloon.scale-0.01;
+    balloon.scale = balloon.scale -0.01;
 
   }
   else if(keyDown(RIGHT_ARROW)){
     updateHeight(10,0);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in right direction
-    balloon.scale = balloon.scale-0.01;
+   balloon.scale = balloon.scale -0.01;
 
   }
   else if(keyDown(UP_ARROW)){
     updateHeight(0,-10);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in up direction
-    balloon.scale = balloon.scale-0.01;
+  balloon.scale = balloon.scale -0.01;
   }
   else if(keyDown(DOWN_ARROW)){
     updateHeight(0,10);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    //write code to move air balloon in down direction
-    balloon.scale = balloon.scale-0.01;
+balloon.scale = balloon.scale -0.01;
   }
 
   drawSprites();
@@ -74,12 +72,12 @@ function readHeight(data){
 
 function updateHeight(x,y){
   database.ref('balloon/height').set({
-    'x' : height.x+x,
-    'y' : height.y+y
+    'x' : height.x + x,
+    'y' : height.y + y
   })
 }
 
 function showError(){
-  console.log("error in writing to the database");
+  console.log("Error in writing to the database");
 
 }
